1d45249c1e128f68460557f0b75157b8e37bf5cb
Anastasiiiii <nastia262002@gmail.com>
2024-01-06 16:15:26 +0200

    add chnges to the function

--- Lab4/index.js
+++ Lab4/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const uuid = require('uuid');
 const express = require('express');
 const onFinished = require('on-finished');
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
+const axios = require('axios');
 const fs = require('fs');
 
 const app = express();
@@ -21,7 +22,7 @@ class Session {
             this.#sessions = JSON.parse(this.#sessions.trim());
 
             console.log(this.#sessions);
-        } catch(e) {
+        } catch (e) {
             this.#sessions = {};
         }
     }
@@ -89,26 +90,24 @@ app.get('/', (req, res) => {
         return res.json({
             username: req.session.username,
             logout: 'http://localhost:3000/logout'
-        })
+        });
     }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
-
-app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
-    res.redirect('/');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const addUser = async (token, mail, user_id, password, username, phone_number) => {
+const addUser = async (token, mail, password, username, phone_number) => {
     let userOptions = {
         method: 'POST',
-        headers: userHeaders,  
-        body: JSON.stringify({
-            email: mail,  
-            user_id: user_id,
+        url: 'https://kpi.eu.auth0.com/api/v2/users',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${token}`
+        },
+        data: JSON.stringify({
+            email: mail,
+            user_id: process.env.CLIENT_ID,
             password: password,
-            connection: "Username-Password-Authentication",
-            authorization: `Bearer ${token}`,
+            connection: 'Username-Password-Authentication',
             user_metadata: {
                 username,
                 phone_number
@@ -119,68 +118,66 @@ const addUser = async (token, mail, user_id, password, username, phone_number) =
         const response = await makeRequest(userOptions);
         console.log('User is added', response.body);
     } catch (error) {
-        console.error('error', error);
+        console.error('Error adding user:', error);
     }
 }
 
-const getToken= async (client_id, client_secret, audience) => {
+const getToken = async () => {
     let options = {
         method: 'POST',
         url: 'https://kpi.eu.auth0.com/oauth/token',
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         form: {
-          client_id: client_id,
-          client_secret: client_secret,
-          audience: audience,
-          grant_type: 'client_credentials'
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            audience: process.env.AUDIENCE,
+            grant_type: 'client_credentials'
         }
     }
     try {
-        const response = await makeRequest(userOptions);
-        console.log('Client`s token is ', response.body);
-    } catch {
-        console.error('error', error);
+        const response = await makeRequest(options);
+        const data = response.body;
+        console.log('Token: ', data.access_token);
+        return data;
+    } catch (error) {
+        console.error('Error getting token:', error);
+        throw error;
     }
 }
 
-const refreshToken = async (client_id, client_secret, refresh_token) => {
-    let options = {
-        method: "POST",
-        url: "https://kpi.eu.auth0.com/oauth/token",
-        headers: { "content-type": "application/x-www-form-urlencoded"},
-        form: {
-          client_id: client_id,
-          client_secret: client_secret,
-          refresh_token: refresh_token,
-          grant_type: "refresh_token",
-        },
-      }
-      try {
-        const response = await makeRequest(userOptions);
-        console.log('The refreshed token is  ', response.body);
-    } catch {
-        console.error('error', error);
-    } 
-}
+const makeRequest = async (options) => {
+    try {
+        const response = await axios(options);
+        return {
+            response: response,
+            body: response.data
+        };
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
 
-// const users = [
-//     {
-//         login: 'Login',
-//         password: 'Password',
-//         username: 'Username',
-//     },
-//     {
-//         login: 'Login1',
-//         password: 'Password1',
-//         username: 'Username1',
-//     }
-// ]
+const users = [
+    {
+        mail: "mail",
+        password: "password",
+        username: "username",
+        phone_number: "1234567"
+    },
+    {
+        mail: "alice4study@gmail.com",
+        password: "D*04i_nA",
+        username: "alice4study@gmail.com",
+        phone_number: "+380682062222"
+    }
+]
 
 app.post('/api/createUser', async (req, res) => {
-    const { token, mail, user_id, password, username, phone_number } = req.body;
-    addUser()
     try {
-        await createUser(token, mail, user_id, password, username, phone_number);
+        const tokenData = await getToken();
+        const token = tokenData.access_token;
+
+        await addUser(token, users[0].mail, users[0].password, users[0].username, users[0].phone_number);
         return res.status(200).json({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error creating user:', error);
@@ -188,6 +185,7 @@ app.post('/api/createUser', async (req, res) => {
     }
 });
 
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
