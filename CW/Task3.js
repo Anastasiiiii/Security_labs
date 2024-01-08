2828f429e8a87a587553180e74e2910e4452e4fa
Anastasiiiii <nastia262002@gmail.com>
2024-01-08 09:44:43 +0200

    add Key encrypt function

--- CW/Task3.js
+++ CW/Task3.js
@@ -4,7 +4,7 @@ const number = "0426";
 const name = "Anastasiia Fartushniak";
 const email = "nastia262002@gmail.com";
 
-console.log("Вільне кодування")
+console.log("Вільне кодування");
 //Вільне кодування
 //Кодування даних
 const encodedNumber = btoa(number);
@@ -41,40 +41,45 @@ console.log("Decoded Number with URL:", decodedNumberWithURL);
 console.log("Decoded Name with URL:", decodedNameWithURL);
 console.log("Decoded Email with URL:", decodedEmailWithURL);
 
-console.log("Симетричне кодування")
+console.log("Симетричне кодування");
 //Симетричне кодування
 const generateKey = () => {
   return crypto.randomBytes(16);
 };
 
-const encryptIDEA = (data, key) => {
-  const cipher = crypto.createCipheriv("idea-ecb", key, Buffer.alloc(0));
+const encryptAES = (data, key) => {
+  const cipher = crypto.createCipheriv("aes-128-cbc", key, Buffer.alloc(16));
   let encrypted = cipher.update(data, "utf-8", "hex");
   encrypted += cipher.final("hex");
   return encrypted;
 };
 
-const decryptIDEA = (encryptedData, key) => {
-  const decipher = crypto.createDecipheriv("idea-ecb", key, Buffer.alloc(0));
+const decryptAES = (encryptedData, key) => {
+  const decipher = crypto.createDecipheriv(
+    "aes-128-cbc",
+    key,
+    Buffer.alloc(16)
+  );
   let decrypted = decipher.update(encryptedData, "hex", "utf-8");
   decrypted += decipher.final("utf-8");
   return decrypted;
 };
 
-const key = generateRandomIDEAKey();
+const key = generateKey();
+console.log("Ключ: ", key);
 
-const encodedNumberWithKey = encryptIDEA(number, key);
-const encodedNameWithKey = encryptIDEA(name, key);
-const encodedEmailWithKey = encryptIDEA(email, key);
+const encryptedNumberWithKey = encryptAES(number, key);
+const encryptedNameWithKey = encryptAES(name, key);
+const encryptedEmailWithKey = encryptAES(email, key);
 
-const decodedNumberWithKey = decryptIDEA(encodedNumberWithKey, key);
-const decodedNameWithKey = decryptIDEA(encodedNameWithKey, key);
-const decodedEmailWithKey = decryptIDEA(encodedEmailWithKey, key);
+const decpyptedNumberWithKey = decryptAES(encryptedNumberWithKey, key);
+const decpyptedNameWithKey = decryptAES(encryptedNameWithKey, key);
+const decpyptedEmailWithKey = decryptAES(encryptedEmailWithKey, key);
 
-console.log(encodedNumberWithKey);
-console.log(encodedNameWithKey);
-console.log(encodedEmailWithKey);
+console.log("Encrypted Number with a Key:", encryptedNumberWithKey);
+console.log("Encrypted Name with a Key:", encryptedNameWithKey);
+console.log("Encrypted Email with a Key:", encryptedEmailWithKey);
 
-console.log(decodedNumberWithKey);
-console.log(decodedNameWithKey);
-console.log(decodedEmailWithKey);
+console.log("Decpypted Number with a Key:", decpyptedNumberWithKey);
+console.log("Decpypted Name with a Key:", decpyptedNameWithKey);
+console.log("Decpypted Email with a Key:", decpyptedEmailWithKey);
